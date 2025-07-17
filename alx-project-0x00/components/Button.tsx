7b0518673db201f3b0d5dace6a6a40c0ee01f986
pitzee <pitzee421@gmail.com>
2025-07-17 20:45:24 +0300

    Advanced Task

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button className={`bg-blue-600 text-white px-4 py-2 ${styles}`}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
