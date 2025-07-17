c9af5c5625b09b16e7dad724e22b2abb7756541d
pitzee <pitzee421@gmail.com>
2025-07-17 20:51:06 +0300

    rounded-lg and rounded full added

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,6 +7,8 @@ const Landing: React.FC = () => {
       <h1 className="text-xl font-extralight">Landing</h1>
       <Card />
       <Button title="small Button" styles="text-sm rounded-sm" />
+      <Button title="small Button" styles="text-sm rounded-lg" />
+      <Button title="small Button" styles="text-sm rounded-full" />
     </div>
   );
 };
