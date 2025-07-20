6976cabdc13e9af0680deac48ba73b154633f72c
pitzee <pitzee421@gmail.com>
2025-07-20 21:10:26 +0300

    Revert "updated"
    This reverts commit 9a34598165d206a7a869d963dc7efcfd43bb49b7.

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -6,17 +6,3 @@ export interface ButtonProps {
   title: string;
   styles: string;
 }
-
-export interface PropertyProps {
-  id: string;
-  title: string;
-  description: string;
-  price: number;
-  location: string;
-  image: string;
-  type: string; // e.g. 'Room', 'Mansion', 'Countryside', etc.
-  bedrooms?: number;
-  bathrooms?: number;
-  amenities?: string[];
-  [key: string]: any; // for any additional dynamic fields
-}
